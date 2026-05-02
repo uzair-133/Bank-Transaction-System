f2efffea502ee7d492c9f0e4e16b17c758703fc7
Uzair Ahmad <uzair247malik@gmail.com>
2026-05-02 11:47:44 +0500

    fixed folder structure

---
+++ frontend/my-bank-app/src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <>
+    <Navbar />  
+    
+    
+    
+    </>
+  )
+}
+
+export default App
