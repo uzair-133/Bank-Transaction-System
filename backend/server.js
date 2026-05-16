51dd5a2be3b4c96a76fdfd25de64a018d0606068
Uzair Ahmad <uzair247malik@gmail.com>
2026-05-16 20:08:34 +0500

    v

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("server is running on port 3000")
 })
+
+module.exports = app;
--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5173","https://ukbankapp.netlify.app/"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true 
+}));
 /**
  * -Routes required
  */
